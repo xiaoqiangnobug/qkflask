eaf16edd5039adf36f9f5f40d14102517d2e1c4f
xiaoqiangnobug <15229360298@163.com>
2019-12-13 11:59:59 +0800

    '单程API搭建'

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask
+from flask import request
+import time
+
+from pc_mian import get_data, get_info
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["POST"])
+def hello_world():
+    HEAD = {
+        "accept": '*/*',
+        # "pre": "87t166w6-7349y6-z4itdq6e-y4md6158-y992n0r1371n",
+        # "pre": "4598deb4-49cb71-444c3e92-2892be11-037a3533c5d1",
+        "pre": "ae01b4af-e10456-140e4857-4303c676-000bfb1e76c5",
+        "cookie": "QN99=5468; QN1=O5cv5V3uLhBEwDpkBpWDAg==; QunarGlobal=10.86.213.149_-5d2b6b2c_16eea5f2445_2b19|1575890448884; QN269=9A8A8701180E11EA95BBFA163E531C43; _i=ueHd8Skw0r-X98gX4Dq-BjsVezAX; QN601=d8dc9670e8f19f0fc86a36373958daa5; QN48=82fe09c0-0eb2-4b83-b77a-f49083bf0914; fid=192fe013-61a7-405a-8092-9eac1baef328; SplitEnv=D; QN621=1490067914133%3DDEFAULT%26fr%3Dqunarindex; Alina=0482a684-1cf590-98446664-34a67222-228acb24d788; QN170=111.18.33.214_3e0094_0_JbA2eqru7o1VGxRySFfw%2BP2azQWhHSqBIiK01pFbIRA%3D; quinn=1265ac7a45f3777f668eb85c68529c14bfebabb879edbf7af1c3aec874ef0e41352c4d1f1168b203ec3e9dc5f7e65ecb; QN66=qunar; QN205=auto_4e0d874a; QN277=auto_4e0d874a; csrfToken=nAN9exzNjvPrTz2SBgEsqDV93yjZFIU4; QN163=0; F235=1575939888029; _vi=c2B3qTmlyoo7YSockUG5GLaFrPq9yn7qHaDiax_QM46x_QbM-6uksVSj7yz_OWfacyGgXkTTqakwh2rYBV05-BOi_cn8aKF7v1i1LkZFGzC5ypPO_gLh2zb6mm3F_-juC8zcbD5wesAhcndU1CVHmMNSI5bGfmXCJFnnFB47twCS; QN300=3w; QN271=747f406a-541a-4594-853c-b70f21edf350; QN267=02105388092ee1e11ae; _abck=b54e88f1-1ab7-47b2-b3d7-dc835a584896~0~1575954272169~-1~-1~-1",
+        "referer": "https://flight.qunar.com/site/interroundtrip_compare.htm",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+
+    }
+
+    # 请求参数
+    data = {
+        "depCity": "北京",
+        "arrCity": "曼谷",
+        "adultNum": 1,
+        "childNum": 0,
+        "from": "qunarindex",
+        "ex_track": '',
+        "_v": 8,
+    }
+    st = int(round(time.time() * 1000))
+
+    if request.method == 'POST':
+
+        depCity = request.form.get('depCity')  # 出发城市
+        arrCity = request.form.get("arrCity")  # 到达城市
+        depDate = request.form.get("depDate")  # 出发日期
+        # retDate = request.form.get("retDate")  # 返回日期
+        adultNum = request.form.get("adultNum") if request.form.get("adultNum") else 1  # 成人数量
+        childNum = request.form.get("childNum") if request.form.get("childNum") else 0  # 小孩数量
+        st = int(round(time.time() * 1000))  # 当前时间戳
+
+        # 还需要判断请求地点是否正确
+
+        if all((depCity, arrCity, depDate)):
+            data.update(
+                {"st": st, "depCity": depCity, "arrCity": arrCity, "depDate": depDate, "adultNum": int(adultNum),
+                 "childNum": int(childNum)})
+            info = get_data(data, HEAD)
+            if type(info) != "str":
+
+                return {"info": get_info(info)}
+
+    return "Data acquisition failed"
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ pc_mian.py
@@ -0,0 +1,160 @@
+import requests
+import time
+import random
+
+t = time.time()
+st = int(round(t * 1000))
+url = "https://flight.qunar.com/touch/api/inter/wwwsearch"
+
+
+# 时间格式处理函数
+def cl_date(d, c):
+    l = d.split(c)
+    return "".join(l)
+
+
+# 航班信息处理函数
+def fenxi_info(i):
+    info = i
+    # 判断是否直达
+    if len(info) == 1:
+        start_info = info[0]
+        info_list = [{
+            "flightNumber": start_info.get('code'),
+            "carrier": start_info.get("carrierCode"),
+            "aircraftCode": start_info.get("planeTypeCode"),
+            'depAirport': start_info.get('depAirportCode'),
+            "depTerminal": start_info.get('depTerminal'),
+            "depTime": cl_date(start_info.get("depDate"), '-') + cl_date(start_info.get("depTime"), ":"),
+            "arrAirport": start_info.get('arrAirportCode'),
+            "arrTime": cl_date(start_info.get("arrDate"), "-") + cl_date(start_info.get("arrTime"), ":"),
+            "arrTerminal": start_info.get('arrTerminal') if start_info.get('arrTerminal') else "",
+            "shared": start_info.get("codeShareStatus"),
+            "realFlightNumber": "",
+            "stopCity": "",
+            "cabins": "V"
+        }]
+
+        if info_list[0]:
+            return {
+                "arrCity": start_info.get("arrCityCode"),
+                "carrier": start_info.get("carrierCode"),
+                "depCity": start_info.get("depCityCode"),
+                "depDate": cl_date(start_info.get("depDate"), '-'),
+                "retDate": "",
+                "meg": "success",
+                "fromSegments": info_list
+            }
+        return {
+                "arrCity": "",
+                "carrier": "",
+                "depCity": "",
+                "depDate": "",
+                "retDate": "",
+                "meg": "fail",
+                "fromSegments": ""
+            }
+    else:
+        # 初始化空列表，不能再for中初始化，避免每次重复
+        info_list = []
+
+        for start_info in info:
+            info_list.append({
+                "flightNumber": start_info.get('code'),
+                "carrier": start_info.get("carrierCode"),
+                "aircraftCode": start_info.get("planeTypeCode"),
+                'depAirport': start_info.get('depAirportCode'),
+                "depTerminal": start_info.get('depTerminal'),
+                "depTime": cl_date(start_info.get("depDate"), '-') + cl_date(start_info.get("depTime"), ":"),
+                "arrAirport": start_info.get('arrAirportCode'),
+                "arrTime": cl_date(start_info.get("arrDate"), "-") + cl_date(start_info.get("arrTime"), ":"),
+                "arrTerminal": start_info.get('arrTerminal') if start_info.get('arrTerminal') else "",
+                "shared": start_info.get("codeShareStatus"),
+                "realFlightNumber": "",
+                "stopCity": "",
+                "cabins": "V"
+            })
+
+        # 判断是否有数据
+        if info[0]:
+            return {
+                "arrCity": info[-1].get("arrCityCode"),
+                "carrier": info[-1].get("carrierCode"),
+                "depCity": info[0].get("depCityCode"),
+                "depDate": cl_date(info[0].get("depDate"), '-'),
+                "retDate": "",
+                "meg": "success",
+                "fromSegments": info_list
+            }
+
+        return {
+                "arrCity": "",
+                "carrier": "",
+                "depCity": "",
+                "depDate": "",
+                "retDate": "",
+                "meg": "fail",
+                "fromSegments": ""
+            }
+
+
+# 请求头
+HEAD = {
+    "accept": '*/*',
+    "cookie": "QN99=5468; QN1=O5cv5V3uLhBEwDpkBpWDAg==; QunarGlobal=10.86.213.149_-5d2b6b2c_16eea5f2445_2b19|1575890448884; QN269=9A8A8701180E11EA95BBFA163E531C43; _i=ueHd8Skw0r-X98gX4Dq-BjsVezAX; QN601=d8dc9670e8f19f0fc86a36373958daa5; QN48=82fe09c0-0eb2-4b83-b77a-f49083bf0914; fid=192fe013-61a7-405a-8092-9eac1baef328; SplitEnv=D; QN621=1490067914133%3DDEFAULT%26fr%3Dqunarindex; Alina=0482a684-1cf590-98446664-34a67222-228acb24d788; QN170=111.18.33.214_3e0094_0_JbA2eqru7o1VGxRySFfw%2BP2azQWhHSqBIiK01pFbIRA%3D; quinn=1265ac7a45f3777f668eb85c68529c14bfebabb879edbf7af1c3aec874ef0e41352c4d1f1168b203ec3e9dc5f7e65ecb; QN66=qunar; QN205=auto_4e0d874a; QN277=auto_4e0d874a; csrfToken=nAN9exzNjvPrTz2SBgEsqDV93yjZFIU4; QN163=0; F235=1575939888029; _vi=c2B3qTmlyoo7YSockUG5GLaFrPq9yn7qHaDiax_QM46x_QbM-6uksVSj7yz_OWfacyGgXkTTqakwh2rYBV05-BOi_cn8aKF7v1i1LkZFGzC5ypPO_gLh2zb6mm3F_-juC8zcbD5wesAhcndU1CVHmMNSI5bGfmXCJFnnFB47twCS; QN300=3w; QN271=747f406a-541a-4594-853c-b70f21edf350; QN267=02105388092ee1e11ae; _abck=b54e88f1-1ab7-47b2-b3d7-dc835a584896~0~1575954272169~-1~-1~-1",
+    "referer": "https://flight.qunar.com/site/interroundtrip_compare.htm",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+
+}
+
+# 请求参数
+data = {
+    "depCity": "北京",
+    "arrCity": "曼谷",
+    "depDate": '2019-12-18',
+    # "retDate": "2019-12-18",
+    "adultNum": 1,
+    "childNum": 0,
+    "from": "qunarindex",
+    "ex_track": '',
+    "_v": 8,
+    "st": st,
+}
+
+
+# 爬取函数
+def get_data(data, head):
+    response = requests.get(url, params=data, headers=head)
+    response.encoding = "utf-8"
+
+    if response.status_code == 200:
+        while True:
+            try:
+                response.json()['code']
+            except:
+                data = response.json().get("result").get("flightPrices")
+                return data if data else "数据解析失败"
+            else:
+                return "数据获取失败!"
+    return "请求失败"
+
+
+info = get_data(data, HEAD)
+
+
+# 单程信息处理函数
+def get_info(xinxi: dict):
+    jipiao = []
+    for i in xinxi.values():
+        jipiao.append(fenxi_info(i.get('journey').get('trips')[0].get("flightSegments")))
+    return jipiao
+
+
+# 往返信息采集函数
+def get_infos(xinxi: dict):
+    jipiao = []
+    for i in xinxi.values():
+        # 航班具体信息
+        jipiao.append({"qu": fenxi_info(i.get('journey').get('trips')[0].get("flightSegments")),
+                       "fan": fenxi_info(i.get('journey').get('trips')[1].get("flightSegments"))})
+    return jipiao
