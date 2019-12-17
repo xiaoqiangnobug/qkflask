28ca854ee523fd2e849a72d344744679e1e01f0f
xiaoqiangnobug <15229360298@163.com>
2019-12-17 13:28:55 +0800

    '修复BUG'

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /usr/src/qkflask
 git pull
-pip install -r requesments.txt -i https://mirrors.aliyun.com/pypi/simple
+pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple
 gunicorn -w 1 -b 0.0.0.0:5000 app:app
